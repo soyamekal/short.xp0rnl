e9bb0eee49eca8bceedde650d8b8f5a73b6b454c
soyamekal <84932124+soyamekal@users.noreply.github.com>
2026-07-13 01:33:44 +0500

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   title: 'ProLink',
-  email: 'juanindrian02@gmail.com',
-  github: 'https://github.com/kencanadev/Sink',
+  
+  
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 24 * 3600, // 24h
