cfef2d89d78dbefd806620b82201e6031af3d23c
soyamekal <84932124+soyamekal@users.noreply.github.com>
2026-07-14 01:33:25 +0500

    Update Footer.vue

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -12,12 +12,12 @@ const { title, email, telegram, blog, twitter, mastodon, github } = useAppConfig
         class="text-xl font-black leading-none text-gray-900 select-none dark:text-gray-100 logo"
         :title="title"
       >{{ title }}</a>
-     
+     <a
         class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0"
-
+/
       >
         &copy; {{ new Date().getFullYear() }} - - Made By Waseem Gul
-      
+      </a>
       <span
         class="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start"
       >
