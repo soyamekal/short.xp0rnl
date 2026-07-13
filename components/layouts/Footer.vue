5a923d5b5da1baacfe46e6b37a4023c321bde3bf
soyamekal <84932124+soyamekal@users.noreply.github.com>
2026-07-14 03:03:45 +0500

    Update Footer.vue

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -7,18 +7,17 @@ const { title, email, telegram, blog, twitter, mastodon, github } = useAppConfig
 <template>
   <section class="md:pt-6">
     <div class="container flex flex-col items-center py-8 mx-auto sm:flex-row">
-      <a
-        href="/"
-        class="text-xl font-black leading-none text-gray-900 select-none dark:text-gray-100 logo"
-        :title="title"
-      >{{ title }}</a>
-     <a
-        
-/
-      
-        &copy; {{ new Date().getFullYear() }} - - Made By Waseem Gul
-      </a>
-      <span
+<a
+  href="/"
+  class="text-xl font-black leading-none text-gray-900 select-none dark:text-gray-100 logo"
+  :title="title"
+>
+  {{ title }}
+</a>
+
+<span class="ml-4 text-sm text-gray-500">
+  &copy; {{ new Date().getFullYear() }} - Made By Waseem Gul
+</span>
         class="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start"
       >
         <a
